0ab05ef8ae9c1703268f7d3aad631dae7a2d0fc4
Shachar Ovadya <ovadya.shachar@gmail.com>
2023-01-07 21:12:29 +0200

    ok

--- EFCore6/Dal/AppDbContext.cs
+++ EFCore6/Dal/AppDbContext.cs
@@ -29,6 +29,27 @@ namespace EFCore6.Dal
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            /* 
+            var studentBuilder = modelBuilder.Entity<StudentDM>();
+            studentBuilder.ToTable("students");
+            studentBuilder.HasKey(dm => dm.Id);
+
+            studentBuilder.Property(dm => dm.FirstName).IsRequired().HasMaxLength(32);
+            studentBuilder.Property(dm => dm.LastName).IsRequired().HasMaxLength(32)
+                .HasDefaultValue("").ValueGeneratedOnAdd();
+            studentBuilder.Property(dm => dm.BirthDate);
+            studentBuilder.Property(dm => dm.Modified).IsRequired()
+                .HasDefaultValueSql("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+                .HasValueGenerator<UtcDateTimeGenerator>() // Only for In memory context
+                .ValueGeneratedOnAddOrUpdate();
+            studentBuilder.Property(dm => dm.Created).IsRequired()
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasValueGenerator<UtcDateTimeGenerator>() // Only for using In memory context
+                .ValueGeneratedOnAddOrUpdate();
+
+            studentBuilder.HasMany(dm => dm.Books).WithOne().HasForeignKey(dm => dm.StudentId);
+            */
         }
 
     }
--- EFCore6/Dal/Models/StudentDM.cs
+++ EFCore6/Dal/Models/StudentDM.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
 using EFCore6.Dal.ValueGenerators;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCore6.Dal.Models
@@ -11,13 +10,25 @@ namespace EFCore6.Dal.Models
     {
         // [Key]
         public uint? Id { get; set; }
-        // [Column("first_name")]
+
+        // [Column("first_name"), Required, MaxLength(32)]
         public string? FirstName { get; set; }
+
+        // [Column("last_name"), Required, MaxLength(32)]
+        // [DefaultValue(""), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string? LastName { get; set; }
+
+        // [Column("birth_date")]
         public DateTime? BirthDate { get; set; }
+
+        // [Column("modified"), Required]
         public DateTime? Modified { get; set; }
+
+        // [Column("created"), Required]
         public DateTime? Created { get; set; }
 
+
+        // [ForeignKey(nameof(StudentBookDM.StudentId))]
         public IEnumerable<StudentBookDM>? Books { get; set; }
     }
 
@@ -29,21 +40,21 @@ namespace EFCore6.Dal.Models
             builder.HasKey(dm => dm.Id);
 
             builder.Property(dm => dm.FirstName).IsRequired().HasMaxLength(32);
-            builder.Property(dm => dm.LastName).IsRequired()
+            builder.Property(dm => dm.LastName).IsRequired().HasMaxLength(32)
                 .HasDefaultValue("").ValueGeneratedOnAdd();
             builder.Property(dm => dm.BirthDate);
             builder.Property(dm => dm.Modified).IsRequired()
-                .HasValueGenerator<UtcDateTimeGenerator>() // Only for In memory context
                 .HasDefaultValueSql("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+                .HasValueGenerator<UtcDateTimeGenerator>() // Only for In memory context
                 .ValueGeneratedOnAddOrUpdate();
             builder.Property(dm => dm.Created).IsRequired()
-                .HasValueGenerator<UtcDateTimeGenerator>() // Only for using In memory context
                 .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .ValueGeneratedOnAdd();
+                .HasValueGenerator<UtcDateTimeGenerator>() // Only for using In memory context
+                .ValueGeneratedOnAddOrUpdate();
 
             builder.HasMany(dm => dm.Books).WithOne().HasForeignKey(dm => dm.StudentId);
         }
     }
 
-    
+
 }
